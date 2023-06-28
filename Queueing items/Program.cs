9fe7069b647667f694e59e3097e12cd4e1f8ae87
rober <rober@DESKTOP-MN9AKPU>
2023-06-28 12:20:44 +0200

    Aggiungere i file di progetto.

---
+++ Queueing items/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("scegli le varie opzioni: \n1)Queue;\n2)Dequeue;\n3)Peek.");
+
+int Option = int.Parse(Console.ReadLine());
+
+Queue<string> ListOfNames = new();
+ListOfNames.Enqueue("Marco");
+ListOfNames.Enqueue("Luca");
+ListOfNames.Enqueue("Anna");
+
+if(Option == 1)     //funziona
+{   
+    Console.Write("inserisci un nuovo nome: ");
+    string NewName = Console.ReadLine();
+
+    ListOfNames.Enqueue(NewName);
+
+    Console.WriteLine("lista completa con il nuovo nome:");
+    foreach (string names in ListOfNames)
+    {
+        Console.WriteLine(names);
+    }
+}
+else if(Option == 2)        //da controllare 
+{
+    string restOfList = ListOfNames.Dequeue();
+    Console.WriteLine("sto rimuovendo il primo nome dalla lista " + restOfList);
+}
+else if(Option == 3)  //funziona
+{
+    string name = ListOfNames.Peek();           
+    Console.WriteLine("sto prendendo il primo nome della lista " + name);
+}
+
+
+
+
+
